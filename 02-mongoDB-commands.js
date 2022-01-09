79e6cd095fd3caeffc9a5d74815f207cc0cfdc86
alvarorl231 <alvaro.roldanlucena@student.hogent.be>
2022-01-10 00:20:13 +0100

    more commands and data

--- 02-mongoDB-commands.js
+++ 02-mongoDB-commands.js
@@ -25,3 +25,47 @@ db.example1.find()
     db.map_reduce_1.find({}).sort({_id: -1})
     db.map_reduce_1.aggregate([{$sort: {_id: -1}},{$project: {_id: 0, Age: "$_id", "Number of people": "$value"}}])
 
+/* Example 2 */
+db.nyse.find().limit(10)
+
+    /* Part 1*/
+    /* Calculate the number of entries for each stock_symbol */
+    var mapFunction = function() {
+        emit(this.stock_symbol, 1);
+    }
+    
+    var reduceFunction = function(key, values){
+        return Array.sum(values);   
+    }
+    
+    db.nyse.mapReduce(mapFunction,reduceFunction, {out: "map_reduce_2"}).find()
+    
+    /* Part 2 */
+    /* Calculate the maximum stock_price_close for each stock_symbol */
+    var mapFunction = function() {
+        emit(this.stock_symbol, this.stock_price_close);
+    };
+    
+    var reduceFunction = function(key, values) {
+        return Math.max(...values);   // spreadoperator 
+    };
+    
+    db.nyse.mapReduce(mapFunction, reduceFunction, { out: "map_reduce_2" }).find()
+    
+    /* Part 3 */
+    /* Calculate the maximum stock_price_close and the date 
+    on which this stock_price_close was reached, for each stock_symbol */
+    var mapFunction = function() {
+        // the value is a tuple (instead of a single value)!
+        emit(this.stock_symbol, {stock_price_close: this.stock_price_close, date: this.date});
+    };
+    
+    var reduceFunction = function(key, values) {
+        // the Javascript reduce function is called for each key!
+        var max = values.reduce((result, value) => {
+             return value.stock_price_close > result.stock_price_close ? value : result 
+        }, {stock_price_close: 0, date: new Date()});
+        return max;
+    };
+    
+    db.nyse.mapReduce(mapFunction, reduceFunction, { out: "map_reduce_2" }).find()
